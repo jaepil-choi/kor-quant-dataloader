dbde17f59e34e7580b928ab0c038e54a24e7a027
jaepil-choi <chljeffreyz@gmail.com>
2024-01-11 06:52:00 +0900

    [FEAT] : Scaffold base Reader interface

--- kor_quant_dataloader/datasource/base.py
+++ kor_quant_dataloader/datasource/base.py
@@ -1,12 +1,29 @@
 from abc import ABC, abstractmethod
 
-class DataSourceInterface(ABC):
-    """Abstract class that defines the interface for a data source.
+import pandas as pd
 
-    """    
+class BaseDataReader(ABC):
+    def __init__(self) -> None:
+        self.
 
     @abstractmethod
-    def __repr__(self):
+    def read(self) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def _fetch_data(self):
+        pass
+    
+    @abstractmethod
+    def _fetch_local_data(self) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def _transform_data(self) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def _show_catalog(self) -> pd.DataFrame:
         pass
 
     
