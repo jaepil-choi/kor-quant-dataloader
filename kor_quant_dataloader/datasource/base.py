33b83da87f8d2cad6c5f26a7229ecd8b941d0ef7
jaepil-choi <chljeffreyz@gmail.com>
2024-01-14 15:43:15 +0900

    [FEAT] : Scaffold Readers (base, pykrx)

--- kor_quant_dataloader/datasource/base.py
+++ kor_quant_dataloader/datasource/base.py
@@ -4,26 +4,52 @@ import pandas as pd
 
 class BaseDataReader(ABC):
     def __init__(self) -> None:
-        self.
+        # self.available_data = []
+        pass
 
     @abstractmethod
-    def read(self) -> pd.DataFrame:
-        pass
+    def read(
+        self,
+        data: str,
+        start_date: str,
+        end_date: str,
+        download: bool
+        ) -> pd.DataFrame:
+        raise NotImplementedError
 
     @abstractmethod
     def _fetch_data(self):
-        pass
+        raise NotImplementedError
     
     @abstractmethod
     def _fetch_local_data(self) -> pd.DataFrame:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get_available_local_dates(self) -> list:
         pass
 
     @abstractmethod
     def _transform_data(self) -> pd.DataFrame:
-        pass
+        raise NotImplementedError
+    
+    @abstractmethod
+    def _remove_holidays(self) -> pd.DataFrame:
+        raise NotImplementedError
 
     @abstractmethod
     def _show_catalog(self) -> pd.DataFrame:
+        raise NotImplementedError
+
+class BaseLocal:
+    def __init__(self) -> None:
         pass
 
-    
+    def read_json(self) -> dict:
+        pass
+
+    def read_hdf(self) -> pd.DataFrame:
+        pass
+
+    def write_hdf(self) -> None:
+        pass
--- kor_quant_dataloader/datasource/pykrx_.py
+++ kor_quant_dataloader/datasource/pykrx_.py
@@ -1,3 +1,4 @@
+from pandas.core.api import DataFrame as DataFrame
 import pykrx as krx
 
 import pandas as pd
@@ -5,5 +6,80 @@ import pandas as pd
 import time
 from tqdm import tqdm
 
-from abc import ABC, abstractmethod
+from kor_quant_dataloader.datasource.base import BaseDataReader
+from kor_quant_dataloader.utils import DateUtil
+
+class PykrxReader(BaseDataReader):
+    def __init__(self) -> None:
+        self.data = None
+        self.start_date = None
+        self.end_date = None
+        self.download = None
+
+        self.date_list = []
+
+    def read(
+        self,
+        data: str,
+        start_date: str,
+        end_date: str,
+        download: bool
+        ) -> pd.DataFrame:
+
+        self.data = data
+        self.start_date = start_date
+        self.end_date = end_date
+        self.download = download
+
+        self.date_list = DateUtil.get_daterange(
+            self.start_date,
+            self.end_date,
+            is_inclusive=True
+            )
+
+        fetched = self._fetch_data_all()
+        processed = self._preprocess_data(fetched)
+
+        return processed
+
+    def _fetch_data_one(
+            self,
+            date: str
+            ) -> pd.DataFrame:
+        pass
+
+    def _fetch_data_all(
+            self,
+            date_list: list
+            ) -> pd.DataFrame:
+        
+        di_snapshots = []
+        for di in tqdm(date_list):
+            di_snapshots.append(self._fetch_data_one(di))
+        
+        df = pd.concat(di_snapshots, axis=0)
+
+        return df
+
+    def _fetch_local_data(
+            self,
+            date_list: list
+            ) -> pd.DataFrame:
+        pass
+
+    def _get_available_local_dates(self) -> list:
+        pass
+
+    def _preprocess_data(self) -> pd.DataFrame:
+        pass
+
+    def _remove_holidays(self) -> DataFrame:
+        pass
+
+    def _show_catalog(self) -> pd.DataFrame:
+        pass
+
+    
+
+
 
